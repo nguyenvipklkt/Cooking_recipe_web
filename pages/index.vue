06442b63c9ee718cec474714cbdcc61bbdab69c4
nguyenvipklkt <nguyenhua2508@gmail.com>
2023-09-24 20:50:27 +0700

    build project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    async beforeCreate() {
+      this.$router.push({path: '/login'})
+    }
+  }
+</script>
+
+<style></style>
---
+++ pages/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="d-flex justify-content-center btn btn-primary" style="font-size: 13px;">   
+        login
+    </div>
+</template>
+  
+<script>
+</script>
+  
+<style>
+</style>
+  
